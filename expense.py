a23af36d4681a909b93ac2aa0ab3693c7adae0c2
Benjamin LE GUEN <benjamin.le-guen@epita.fr>
2022-11-02 22:07:01 +0100

    Final push

--- expense.py
+++ expense.py
@@ -1,5 +1,7 @@
 from PyInquirer import prompt
+import csv
 
+users = []
 expense_questions = [
     {
         "type":"input",
@@ -11,20 +13,59 @@ expense_questions = [
         "name":"label",
         "message":"New Expense - Label: ",
     },
-    {
-        "type":"input",
-        "name":"spender",
-        "message":"New Expense - Spender: ",
-    },
-
 ]
 
-
+spender_questions = [
+    {
+        "type":"list",
+        "name":"spender",
+        "message": "Who is the spender ?",
+        "choices": users,
+    }
+]
 
 def new_expense(*args):
     infos = prompt(expense_questions)
     # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
+    if users == []:
+        with open('users.csv', 'r') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                users.append(row[0])
+
+
+    infos2 = prompt(spender_questions)
+    with open('expense_report.csv', 'a+', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=" ")
+        writer.writerow([infos["amount"]] + [infos["label"]] + [infos2["spender"]])
     print("Expense Added !")
+    
+    #############################################################################################
+    # Add in the file "users_info.csv" the name of users and the amount of money after spliting #
+    #############################################################################################
+
+    open('users_info.csv', 'a+', newline='')
+    with open('users_info.csv', 'r+', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=" ")
+        reader = csv.reader(csvfile)
+        data = list(csvfile)
+        finaldata = []
+        for i in range(len(data)):
+            string = data[i].split()
+            finaldata.append(str(string[1]))
+        if data == []:
+            for i in range(len(users)):            
+                    if users[i] == infos2["spender"]:
+                        writer.writerow([users[i]] + [int(infos["amount"]) - int(infos["amount"]) / (len(users))])
+                    else :
+                        writer.writerow([users[i]] + [-1 * int(infos["amount"]) / (len(users))])
+        else:
+            open("users_info.csv", "w").close()
+            for i in range(len(users)):            
+                    if users[i] == infos2["spender"]:
+                        writer.writerow([users[i]] + [(int(infos["amount"]) - int(infos["amount"]) / (len(users))) + float(finaldata[i])])
+                    else :
+                        writer.writerow([users[i]] + [(-1 * int(infos["amount"]) / (len(users))) + float(finaldata[i])])
     return True
 
 
--- user.py
+++ user.py
@@ -1,8 +1,18 @@
 from PyInquirer import prompt
+import csv
 user_questions = [
-
+    {
+        "type":"input",
+        "name":"name",
+        "message":"What is your name ?",
+    },
 ]
 
 def add_user():
     # This function should create a new user, asking for its name
-    return
+    infos = prompt(user_questions)
+    with open('users.csv', 'a+', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=" ")
+        writer.writerow([infos["name"]])
+    print("New user named " + infos["name"] + " was created")
+    return True
